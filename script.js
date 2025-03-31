a93f7ad63f6f81dcbc5187c570ec5e88da79225f
SHRISTI <157280101+Shristi0124@users.noreply.github.com>
2025-04-01 02:47:19 +0530

    change

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 let prompt = document.querySelector("#prompt")
+let chatContainer = document.querySelector(".chat-container")
 
+function createChatBox(html,classes){
+    let div = document.createElement("div")
+    div.innerHTML = html
+    div.classList.add(classes)
+    return div
 
-prompt.addEventListener("keydown", (e)=>{
-    if(e.key=="Enter"){
-        console.log(prompt.value);
+}
+
+function handelechatResponse(message) {
+    let html = `<img src="user.jpg" alt="" id="UserImage" width="50" height="50">
+
+    <div class="user-chat-area">
+    ${message}         
+    </div>`
+    prompt.value = ""
+    let userChatBox = createChatBox(html,"user-chat-box")
+    chatContainer.appendChild(userChatBox)
+
+    setTimeout(()=>{
+        let html = `            <img src="bot.jpg" alt="" id="aiImage" width="50" height="40">
+
+            <div class="ai-chat-area">
+                   <img src="loading.gif" alt=""   class ="load" width="50px">
+            
+        
+            </div>`
+            let aiChatBox=createChatBox(html,"ai-chat-box")
+            chatContainer.appendChild(aiChatBox)
+
+    },600)
+}
+
+prompt.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+        handelechatResponse(prompt.value)
 
 
     }
-    console.log(e)
-  
-
 })
